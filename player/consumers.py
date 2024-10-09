0efc6be5a3f4d944f969750e76fe26b12cfe3b36
Amirhossein Khalili <amirhosseinkhalili901@gmail.com>
2024-10-09 21:59:32 +0330

    add watched history

--- player/consumers.py
+++ player/consumers.py
@@ -1,5 +1,9 @@
 import json
+
+from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
+
+from player.models import Video, VideoWatchHistory
 
 
 class VideoConsumer(AsyncWebsocketConsumer):
@@ -8,10 +12,10 @@ class VideoConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.video_id = self.scope['url_route']['kwargs']['video_id']
         self.group_name = f'video_{self.video_id}'
+        user = self.scope["user"]
 
         VideoConsumer.viewers[self.video_id] = VideoConsumer.viewers.get(self.video_id, 0) + 1
 
-        # Add the client to the group
         await self.channel_layer.group_add(
             self.group_name,
             self.channel_name
@@ -19,37 +23,34 @@ class VideoConsumer(AsyncWebsocketConsumer):
 
         await self.accept()
 
-        # Send the current viewer count to the new client
+        if user.is_authenticated:
+            video = await database_sync_to_async(self.get_video)(self.video_id)
+            await database_sync_to_async(self.create_watch_history)(user, video)
+
         await self.send_viewer_count()
 
-        # Broadcast the viewer count to all clients in this video group
         await self.broadcast_viewer_count()
 
     async def disconnect(self, close_code):
-        # Decrement the viewer count for this video
         if self.video_id in VideoConsumer.viewers:
             VideoConsumer.viewers[self.video_id] -= 1
             if VideoConsumer.viewers[self.video_id] <= 0:
                 del VideoConsumer.viewers[self.video_id]
 
-        # Remove the client from the group
         await self.channel_layer.group_discard(
             self.group_name,
             self.channel_name
         )
 
-        # Broadcast the updated viewer count to all clients in this video group
         await self.broadcast_viewer_count()
 
     async def send_viewer_count(self):
-        # Send the viewer count for this video to the current client
         viewer_count = VideoConsumer.viewers.get(self.video_id, 0)
         await self.send(text_data=json.dumps({
             'viewers': viewer_count
         }))
 
     async def broadcast_viewer_count(self):
-        # Broadcast the viewer count to all connected clients in this video group
         viewer_count = VideoConsumer.viewers.get(self.video_id, 0)
         await self.channel_layer.group_send(
             self.group_name,
@@ -60,7 +61,12 @@ class VideoConsumer(AsyncWebsocketConsumer):
         )
 
     async def send_viewer_count_to_all(self, event):
-        # Send the viewer count to all clients in the group
         await self.send(text_data=json.dumps({
             'viewers': event['viewers']
         }))
+
+    def get_video(self, video_id):
+        return Video.objects.get(id=video_id)
+
+    def create_watch_history(self, user, video):
+        VideoWatchHistory.objects.create(user=user, video=video)
--- player/models.py
+++ player/models.py
@@ -1,5 +1,8 @@
+from django.contrib.auth import get_user_model
 from django.core.validators import FileExtensionValidator
 from django.db import models
+
+User = get_user_model()
 
 
 class Video(models.Model):
@@ -12,3 +15,17 @@ class Video(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class VideoWatchHistory(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video = models.ForeignKey(Video, on_delete=models.CASCADE)
+    watched_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        constraints = [
+            models.UniqueConstraint(fields=['user', 'video'], name='unique_user_video')
+        ]
+
+    def __str__(self):
+        return f"{self.user} watched {self.video} at {self.watched_at}"
